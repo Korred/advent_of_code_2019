05613ef7dd06113ea0cf3e29fe2664168929af52
Philipp Kochanski <P.Kochanski@uni-duesseldorf.de>
2019-12-05 12:06:06 +0100

    Day 4 refactoring

--- day_04/task_1.py
+++ day_04/task_1.py
@@ -1,24 +1,18 @@
 inp = '183564-657474'
 start_range, end_range = map(int, inp.split('-'))
 
-possible_passwords = []
 
-for password in range(start_range, end_range + 1):
-    str_pw = str(password)
-    invalid_pw = False
-    adjacent_found = False
+def check_conditions(pwd):
+    # order should remain the same if going from left to right, the digits never decrease;
+    cond1 = list(pwd) == sorted(pwd)
 
-    # Going from left to right, the digits never decrease;
-    for a, b in zip(str_pw, str_pw[1:]):
-        if int(a) > int(b):
-            invalid_pw = True
-            break
+    # by using set() we can find it there are repetitions
+    # condition 1 ensures ascending order, so pwds like '410524' -> {0, 1, 2, 4, 5} will not be accepted
+    cond2 = len(set(pwd)) < len(pwd)
 
-        if a == b:
-            adjacent_found = True
+    return all([cond1, cond2])
 
-    if not invalid_pw and adjacent_found:
-        possible_passwords.append(str_pw)
 
+possible_passwords = [pwd for pwd in range(start_range, end_range + 1) if check_conditions(str(pwd))]
 
 print(len(possible_passwords))
--- day_04/task_2.py
+++ day_04/task_2.py
@@ -1,35 +1,24 @@
 import re
 
-inp = '183564-657474'
-start_range, end_range = map(int, inp.split('-'))
-
-possible_passwords = []
-
 pattern_1 = re.compile(r'(.)\1{2,}')
 pattern_2 = re.compile(r'(.)\1{1}')
 
-for password in range(start_range, end_range + 1):
-    str_pw = str(password)
-    invalid_pw = False
-    adjacent_found = False
+inp = '183564-657474'
+start_range, end_range = map(int, inp.split('-'))
 
-    # Going from left to right, the digits never decrease;
-    for a, b in zip(str_pw, str_pw[1:]):
-        if int(a) > int(b):
-            invalid_pw = True
-            break
 
-    # the two adjacent matching digits are not part of a larger group of matching digits
-    if not invalid_pw:
-        # remove larger groups first
-        sub_pw = pattern_1.sub('', str_pw)
+def check_conditions(pwd):
+    # order should remain the same if going from left to right, the digits never decrease;
+    cond1 = list(pwd) == sorted(pwd)
 
-        # check if two adjacent matcheing digits remain
-        if pattern_2.search(sub_pw):
-            adjacent_found = True
+    # remove larger groups first
+    # check if two adjacent matcheing digits remain
+    cond2 = pattern_2.search(pattern_1.sub('', pwd))
 
-    if not invalid_pw and adjacent_found:
-        possible_passwords.append(str_pw)
+    return all([cond1, cond2])
 
+
+possible_passwords = [pwd for pwd in range(start_range, end_range + 1) if check_conditions(str(pwd))]
 
 print(len(possible_passwords))
+
